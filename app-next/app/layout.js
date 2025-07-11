e9eea042b0d7354a4db40b1fee56cf6a8865b816
mateus_britto <mateusbritto00@gmail.com>
2025-07-11 15:04:51 +0200

    Change the title and add a description to the metadata

--- app-next/app/layout.js
+++ app-next/app/layout.js
@@ -1,7 +1,9 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "HackYourFuture",
+  title: "Meal-Sharing",
+  description:
+    "Discover and share delicious meals with the Meal-Sharing app. Find, host, and enjoy home-cooked dishes from around the world. Join our community and explore new culinary experiences.",
 };
 
 export default function RootLayout({ children }) {
